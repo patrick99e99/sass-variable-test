b8675b18ead1f944dccf6ec999de78edae92ac30
Patrick Collins <patrick@collinatorstudios.com>
2021-11-24 11:52:55 -0700

    why does this not work

---
+++ public/packs/js/application.js
@@ -0,0 +1,32 @@
+(self["webpackChunksass_variable_test"] = self["webpackChunksass_variable_test"] || []).push([["application"],{
+
+/***/ "./app/javascript/entrypoints/application.js":
+/*!***************************************************!*\
+  !*** ./app/javascript/entrypoints/application.js ***!
+  \***************************************************/
+/***/ ((__unused_webpack_module, __webpack_exports__, __webpack_require__) => {
+
+"use strict";
+__webpack_require__.r(__webpack_exports__);
+/* harmony import */ var _stylesheets_application_scss__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! ../stylesheets/application.scss */ "./app/javascript/stylesheets/application.scss");
+/* harmony import */ var _stylesheets_application_scss__WEBPACK_IMPORTED_MODULE_0___default = /*#__PURE__*/__webpack_require__.n(_stylesheets_application_scss__WEBPACK_IMPORTED_MODULE_0__);
+
+
+/***/ }),
+
+/***/ "./app/javascript/stylesheets/application.scss":
+/*!*****************************************************!*\
+  !*** ./app/javascript/stylesheets/application.scss ***!
+  \*****************************************************/
+/***/ (() => {
+
+throw new Error("Module build failed (from ./node_modules/mini-css-extract-plugin/dist/loader.js):\nHookWebpackError: Module build failed (from ./node_modules/sass-loader/dist/cjs.js):\nSassError: Undefined variable.\n  ╷\n2 │   color: $color;\n  │          ^^^^^^\n  ╵\n  app/javascript/stylesheets/lol.scss 2:10         @use\n  app/javascript/stylesheets/application.scss 2:1  root stylesheet\n    at tryRunOrWebpackError (/Users/patrick/coding/ruby/rails/sass-variable-test/node_modules/webpack/lib/HookWebpackError.js:88:9)\n    at __webpack_require_module__ (/Users/patrick/coding/ruby/rails/sass-variable-test/node_modules/webpack/lib/Compilation.js:4979:12)\n    at __webpack_require__ (/Users/patrick/coding/ruby/rails/sass-variable-test/node_modules/webpack/lib/Compilation.js:4936:18)\n    at /Users/patrick/coding/ruby/rails/sass-variable-test/node_modules/webpack/lib/Compilation.js:5007:20\n    at symbolIterator (/Users/patrick/coding/ruby/rails/sass-variable-test/node_modules/neo-async/async.js:3485:9)\n    at done (/Users/patrick/coding/ruby/rails/sass-variable-test/node_modules/neo-async/async.js:3527:9)\n    at Hook.eval [as callAsync] (eval at create (/Users/patrick/coding/ruby/rails/sass-variable-test/node_modules/tapable/lib/HookCodeFactory.js:33:10), <anonymous>:15:1)\n    at Hook.CALL_ASYNC_DELEGATE [as _callAsync] (/Users/patrick/coding/ruby/rails/sass-variable-test/node_modules/tapable/lib/Hook.js:18:14)\n    at /Users/patrick/coding/ruby/rails/sass-variable-test/node_modules/webpack/lib/Compilation.js:4914:43\n    at symbolIterator (/Users/patrick/coding/ruby/rails/sass-variable-test/node_modules/neo-async/async.js:3482:9)\n-- inner error --\nError: Module build failed (from ./node_modules/sass-loader/dist/cjs.js):\nSassError: Undefined variable.\n  ╷\n2 │   color: $color;\n  │          ^^^^^^\n  ╵\n  app/javascript/stylesheets/lol.scss 2:10         @use\n  app/javascript/stylesheets/application.scss 2:1  root stylesheet\n    at Object.<anonymous> (/Users/patrick/coding/ruby/rails/sass-variable-test/node_modules/css-loader/dist/cjs.js??ruleSet[1].rules[3].use[1]!/Users/patrick/coding/ruby/rails/sass-variable-test/node_modules/sass-loader/dist/cjs.js??ruleSet[1].rules[3].use[2]!/Users/patrick/coding/ruby/rails/sass-variable-test/node_modules/sass-loader/dist/cjs.js??ruleSet[1].rules[5].use[0]!/Users/patrick/coding/ruby/rails/sass-variable-test/app/javascript/stylesheets/application.scss:1:7)\n    at /Users/patrick/coding/ruby/rails/sass-variable-test/node_modules/webpack/lib/javascript/JavascriptModulesPlugin.js:432:11\n    at Hook.eval [as call] (eval at create (/Users/patrick/coding/ruby/rails/sass-variable-test/node_modules/tapable/lib/HookCodeFactory.js:19:10), <anonymous>:7:1)\n    at Hook.CALL_DELEGATE [as _call] (/Users/patrick/coding/ruby/rails/sass-variable-test/node_modules/tapable/lib/Hook.js:14:14)\n    at /Users/patrick/coding/ruby/rails/sass-variable-test/node_modules/webpack/lib/Compilation.js:4981:39\n    at tryRunOrWebpackError (/Users/patrick/coding/ruby/rails/sass-variable-test/node_modules/webpack/lib/HookWebpackError.js:83:7)\n    at __webpack_require_module__ (/Users/patrick/coding/ruby/rails/sass-variable-test/node_modules/webpack/lib/Compilation.js:4979:12)\n    at __webpack_require__ (/Users/patrick/coding/ruby/rails/sass-variable-test/node_modules/webpack/lib/Compilation.js:4936:18)\n    at /Users/patrick/coding/ruby/rails/sass-variable-test/node_modules/webpack/lib/Compilation.js:5007:20\n    at symbolIterator (/Users/patrick/coding/ruby/rails/sass-variable-test/node_modules/neo-async/async.js:3485:9)\n\nGenerated code for /Users/patrick/coding/ruby/rails/sass-variable-test/node_modules/css-loader/dist/cjs.js??ruleSet[1].rules[3].use[1]!/Users/patrick/coding/ruby/rails/sass-variable-test/node_modules/sass-loader/dist/cjs.js??ruleSet[1].rules[3].use[2]!/Users/patrick/coding/ruby/rails/sass-variable-test/node_modules/sass-loader/dist/cjs.js??ruleSet[1].rules[5].use[0]!/Users/patrick/coding/ruby/rails/sass-variable-test/app/javascript/stylesheets/application.scss\n1 | throw new Error(\"Module build failed (from ./node_modules/sass-loader/dist/cjs.js):\\nSassError: Undefined variable.\\n  ╷\\n2 │   color: $color;\\n  │          ^^^^^^\\n  ╵\\n  app/javascript/stylesheets/lol.scss 2:10         @use\\n  app/javascript/stylesheets/application.scss 2:1  root stylesheet\");");
+
+/***/ })
+
+},
+/******/ __webpack_require__ => { // webpackRuntimeModules
+/******/ var __webpack_exec__ = (moduleId) => (__webpack_require__(__webpack_require__.s = moduleId))
+/******/ var __webpack_exports__ = (__webpack_exec__("./app/javascript/entrypoints/application.js"));
+/******/ }
+]);
